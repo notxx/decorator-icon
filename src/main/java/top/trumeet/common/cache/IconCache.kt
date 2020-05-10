0fabfa3f805e073ad6b01a3045cc75742d61d427
notXX <notxx@qq.com>
2020-05-10 23:24:30 +0800

    对圆环图标重新采样

--- src/main/java/top/trumeet/common/cache/IconCache.kt
+++ src/main/java/top/trumeet/common/cache/IconCache.kt
@@ -4,6 +4,7 @@ import kotlin.math.hypot
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Rect
 import android.graphics.RectF
@@ -52,7 +53,13 @@ class IconCache private constructor() {
 					} else {
 						// Log.d("SmallIcon", "legacy foreground ${icon.getIntrinsicWidth()}, ${icon.getIntrinsicHeight()}")
 						bitmap = ImgUtils.drawableToBitmap(icon, BOUNDS, BOUNDS.width(), BOUNDS.height())
-						removeBackground(bitmap)
+						if (removeBackground(bitmap)) {
+							val temp = Bitmap.createBitmap(32, 32, Bitmap.Config.ARGB_8888);
+							val canvas = Canvas(temp);
+							canvas.drawBitmap(bitmap, null, Rect(-20, -20, 52, 52), null)
+							bitmap.recycle()
+							bitmap = temp
+						}
 					}
 					return bitmap
 				} catch (ignored:Exception) {
@@ -188,8 +195,8 @@ class IconCache private constructor() {
 			}
 		}
 
-		@JvmStatic fun removeBackground(bitmap:Bitmap?, dest:Int = Color.TRANSPARENT) {
-			if (bitmap == null) { return }
+		@JvmStatic fun removeBackground(bitmap:Bitmap?, dest:Int = Color.TRANSPARENT):Boolean {
+			if (bitmap == null) { return false }
 
 			val width = bitmap.getWidth(); val height = bitmap.getHeight()
 			val pixels = IntArray(width * height)
@@ -200,12 +207,11 @@ class IconCache private constructor() {
 			// 方形
 			val lt = bitmap.getPixel(0, 0); val rt = bitmap.getPixel(width - 1, 0)
 			val lb = bitmap.getPixel(0, height - 1); val rb = bitmap.getPixel(width - 1, height - 1)
-			if ((lt == rt) && (rt == lb) && (lb == rb)) { // 四角颜色一致
-				if (lt != dest) {
-					// Log.d("SmallIcon", "removeBackground1($pixels, $width, $height, ${Integer.toHexString(lt)}, ${Integer.toHexString(dest)})")
-					removeBackground0(pixels, width, height, lt, dest)
-					return
-				}
+			if ((lt == rt) && (rt == lb) && (lb == rb) && (lt != dest)) { // 四角颜色一致
+				// Log.d("SmallIcon", "removeBackground1($pixels, $width, $height, ${Integer.toHexString(lt)}, ${Integer.toHexString(dest)})")
+				removeBackground0(pixels, width, height, lt, dest)
+				bitmap.setPixels(pixels, 0, width, 0, 0, width, height)
+				return false
 			}
 
 			// 圆形
@@ -218,17 +224,20 @@ class IconCache private constructor() {
 			if (maxBy != null && maxBy.value > 300) { // TODO
 				// Log.d("SmallIcon", "removeBackground1($pixels, $width, $height, ${Integer.toHexString(lt)}, ${Integer.toHexString(dest)})")
 				removeColor(pixels, width, height, maxBy.key, dest) // TODO
+				bitmap.setPixels(pixels, 0, width, 0, 0, width, height)
+				return true
 			}
 
 			bitmap.setPixels(pixels, 0, width, 0, 0, width, height)
+			return false
 		}
 
 		// 环状扫描
-		fun circularScan(pixels:IntArray, width:Int, height:Int, map:MutableMap<Int, Int>, from:Float, to:Float = from + 1) {
+		fun circularScan(pixels:IntArray, width:Int, height:Int, map:MutableMap<Int, Int>, from:Float, to:Float = from + 1):Int {
 			assert(from > to)
 			// Log.d("SmallIcon", "width,height,cx,cy,r = $width, $height, $cx, $cy, $r")
 
-			val cx = width.toFloat() / 2; val cy = height.toFloat() / 2
+			val cx = width.toFloat() / 2; val cy = height.toFloat() / 2; var count = 0
 			for (y in 0 until height) {
 				for (x in 0 until width) {
 					val hypot = hypot(x - cx, y - cy)
@@ -237,6 +246,7 @@ class IconCache private constructor() {
 					val pos = width * y + x // 偏移
 					val pixel = pixels[pos] // 颜色值
 					if (pixel == Color.TRANSPARENT) continue
+					count++
 					if (map.containsKey(pixel)) {
 						var count = map[pixel]
 						if (count != null) { map[pixel] = count + 1 }
@@ -245,6 +255,7 @@ class IconCache private constructor() {
 					}
 				}
 			}
+			return count
 		}
 
 		private val STATE_SWING:Byte = 0.toByte() // 未决
@@ -301,9 +312,17 @@ class IconCache private constructor() {
 
 		// 直接把特定颜色移除
 		fun removeColor(pixels:IntArray, width:Int, height:Int, target:Int, dest:Int = Color.TRANSPARENT) {
+			val r = ((target and 0x00FF0000) shr 16) // 红色
+			val g = ((target and 0x0000FF00) shr 8) // 绿色
+			val b = (target and 0x000000FF) // 蓝色
 			for (pos in 0 until pixels.size) { // 正向填充
 				val pixel = pixels[pos]
-				if (pixel == target) {
+				if (pixel == Color.TRANSPARENT) continue
+				val red = ((pixel and 0x00FF0000) shr 16) - r // 红色差异
+				val green = ((pixel and 0x0000FF00) shr 8) - g // 绿色差异
+				val blue = (pixel and 0x000000FF) - b // 蓝色差异
+				val diff = red * red + green * green + blue * blue
+				if (diff <= 256) {
 					pixels[pos] = dest
 				}
 			}
